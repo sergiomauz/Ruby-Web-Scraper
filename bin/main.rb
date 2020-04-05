8a0c7f29e6390a7de1b29a5e4b3a7ecab565be2f
Sergio <sergio.mauz88@gmail.com>
2020-04-05 15:54:13 -0500

    Added generic printer method

---
+++ bin/main.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/env ruby
+
+def print_result()
+  ra = "\n-------------"
+  ra += "\nQ: What is Lorem Ipsum?"
+  ra += "\nD: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letra..."
+  ra += "\nTags: Lorem, Ipsum, Dummy, Text"
+  ra += "\nVotes: 100 | Answers: 100"
+  ra += "\nDate: asked Jun 10' 20"
+  ra += "\nAuthor: John Doe"
+  ra += "\nVisit: https://localhost.xyz"
+  ra += "\n-------------"
+  ra
+end    
+
+
+
+
+
+
